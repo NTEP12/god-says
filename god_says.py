cfaa3c795af11264bfe9dbfbd00c0d2b6faa431c
ntep12 <ntep12@protonmail.ch>
2021-12-05 17:01:28 +0300

    god says...

--- god_says.py
+++ god_says.py
@@ -1,5 +1,5 @@
 import random
-f = open('g0dsays/godswords', 'r')
+f = open('godswords', 'r')
 gods_words = f.read()
 x = ''
 for i in range(0,23): x = x + (random.choice(gods_words.split(' '))) + ' '
